922ec6dde7e8cba052dfbcbf441aa86d421df8ca
IU7 <aolenev@bmstu.ru>
2022-04-25 02:09:01 +0300

    FIX: some magic numbers in progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,13 +5,14 @@ GAME_RULES = 'What number is missing in the progression?'
 GAME_NAME = 'progression'
 _NUMBER_MAX = 10
 _NUMBER_MIN = 1
+_STEP_MAX = 10
 _PROGRESSION_LEN_MIN = 5
 
 
 def generate_question():
-    start = int(random() * 100)
-    step = int(random() * 10) + 1
-    end = step * (int(random() * 5) + _PROGRESSION_LEN_MIN) + start
+    start = int(random() * _NUMBER_MIN)
+    step = int(random() * _STEP_MAX) + 1
+    end = step * (int(random() * _STEP_MAX) + _PROGRESSION_LEN_MIN) + start
     progression = list(range(start, end, step))
     x_index = int(random() * (len(progression) - 1)) + 1
 
