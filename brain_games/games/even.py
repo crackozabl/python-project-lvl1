3d196fb76ec39f0b66cd99f5b2d7f816ce3d4948
IU7 <aolenev@bmstu.ru>
2022-04-25 02:11:15 +0300

    FIX: magic numbers in even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -3,10 +3,12 @@ from random import random
 
 GAME_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 GAME_NAME = 'even'
+_NUMBER_MAX = 100
+_NUMBER_MIN = 1
 
 
 def generate_question():
-    question_number = int(random() * 100)
+    question_number = int(random() * _NUMBER_MAX) + _NUMBER_MIN
 
     return (str(question_number), 'yes' if question_number % 2 == 0 else 'no')
 
